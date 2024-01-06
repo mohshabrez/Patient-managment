b2cc55d7402c53a1aabd093e1aa2d3d8b64312a7
Shabrez G <31725739+mohshabrez@users.noreply.github.com>
2024-01-06 19:18:00 +0530

    Update Main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { NavBar } from './components/NavBar';
+import { Route, Routes } from 'react-router-dom';
+import { PatientView } from './components/PatientView';
+import { PatientForm } from './components/PatientForm';
+import { PatientDetail } from './components/PatientDetail';
+import { WardView } from './components/WardView';
+import { WardDetail } from './components/WardDetail';
+import { WardForm } from './components/WardForm';
+import { HospitalView } from './components/HospitalView';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-gray-800 min-h-screen">
+      <NavBar/>
+      <Routes>
+        <Route path='/' element={<PatientView/>}/>
+        <Route path='/patients/:id' element={<PatientDetail/>}/>
+        <Route path='/patients/add' element={<PatientForm/>}/>
+        <Route path='/patients/edit/:id' element={<PatientForm/>}/>
+        <Route path='/wards' element={<WardView/>}/>
+        <Route path='/wards/:id' element={<WardDetail/>}/>
+        <Route path='/wards/add' element={<WardForm/>}/>
+        <Route path='/wards/edit/:id' element={<WardForm/>}/>
+        <Route path='/hospitals' element={<HospitalView/>}/>
+      </Routes>
     </div>
   );
 }
